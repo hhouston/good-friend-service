06a49f32880dac245225e416439749c03d3285ad
Hunter Houston <hmhouston7@gmail.com>
2020-10-12 22:01:51 -0500

    Add /login and add user to gql context

--- server.js
+++ server.js
@@ -14,8 +14,12 @@ import { scopePerRequest } from 'awilix-koa'
 import { typeDefs } from './lib/graphql'
 
 import { createDependencies } from './lib'
-import { UserService } from './lib/services'
+import { MongoService } from './lib/services'
+import { createMongoClient } from './lib/clients'
 import { validateToken } from './lib/services'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import { isNil, head, isEmpty } from 'ramda'
 
 const dependencies = createDependencies()
 
@@ -23,16 +27,38 @@ const app = new Koa()
 const router = new Router()
 const port = config.get('port')
 const jwtSecret = config.get('jwtSecret')
+const mongo = config.get('mongo')
 
 router.post('/login', koaBody(), async (ctx) => {
-  const body = ctx.request.body
+  try {
 
-  if (body.type == 'email') {
-    const { email } = body
-    const token = await UserService.login({ email })
+    const { email, password, type } = ctx.request.body
+    const mongoClient = createMongoClient({ url: mongo.url })
+    const db = await mongoClient()
+    const table = db.collection('user')
+    const query = { email }
+    const cursor = isNil(query) ? await table.find() : await table.find(query)
+
+    const items = await cursor.toArray()
+    if (isEmpty(items)) {
+      throw new Error(`No user Found with Email: ${email}`)
+    }
+    const user = head(items)
+    const isValid = await bcrypt.compare(password, user.password)
+    if (!isValid) {
+      throw new Error('Incorrect password')
+    }
+
+    const token = jwt.sign({ email }, jwtSecret, {
+      subject: email,
+      expiresIn: '14d'
+    })
+
+    ctx.body = { token }
+  } catch (err) {
+    // this.log.error({ err }, 'Login User: Fail')
+    throw err
   }
-
-  ctx.body = { token }
 })
 
 router.post('/signup', koaBody(), (ctx) => {
@@ -53,7 +79,16 @@ const server = new ApolloServer({
     Photo: dependencies.resolve('Photo'),
     Upload: GraphQLUpload
   },
-  // context: ({ ctx }) => validateToken({ jwtSecret, token: ctx.request.header }),
+  context: ({ ctx }) => {
+    const user = validateToken({
+      jwtSecret,
+      token: ctx.request.header
+    })
+
+    console.log('user::: ', user);
+
+    return { user }
+  },
   onHealthCheck: () => {
     return new Promise((resolve, reject) => {
       // Replace the `true` in this conditional with more specific checks!
